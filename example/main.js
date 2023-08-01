2730479cb1a332b803804760577ed57789acdd52
absqueued <3298585+absqueued@users.noreply.github.com>
2023-08-02 01:52:49 +0200

    making example work

--- example/main.js
+++ example/main.js
@@ -1,4 +1,8 @@
 const { foo, bar } = require('./foobar');
 const { baz, qux } = require('./bazqux');
 
+const abc = 'Abc';
+
+const obj = { a: 'a', b: 'b', c: 'c' };
+
 console.log(foo(), bar(), baz(), qux());
